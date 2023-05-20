24104ff0e45371445752d0815e0da9a0e52d7d6e
WeddingElm160 <weddingelm160@outlook.com>
2023-05-20 01:05:44 -0600

    Correccion de images y actualización de packages

--- constants/Images.js
+++ constants/Images.js
@@ -1,19 +1,10 @@
 // local imgs 
+const Onboarding = require('../assets/imgs/getPro-bg-1.png');
+const Logo = require('../assets/imgs/Logo-App.svg')
 import React, { Component } from "react";
 import { Image, Svg } from "react-native";
 
-class Logo extends Component {
-  render() {
-    return (
-      <Image
-        source={require("../assets/imgs/Logo-App.svg")}
-        width={100}
-        height={100}
-      />
-    );
-  }
-}
-
 export default {
-  Logo
+  Logo,
+  Onboarding
 };
