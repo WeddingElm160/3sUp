e732772b7d4a54203926be97d08519b401d577df
WeddingElm160 <weddingelm160@outlook.com>
2023-05-19 23:39:06 -0600

    Cambios generales

--- navigation/Screens.js
+++ navigation/Screens.js
@@ -4,7 +4,14 @@ import { Text } from 'react-native';
 import { Header } from '../components';
 import { nowTheme } from '../constants';
 // drawer
-import CustomDrawerContent from './Menu';
+//import CustomDrawerContent from './Menu';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const { width } = Dimensions.get("window");
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 function screens() {
   return (
