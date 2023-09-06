a093d003726a1e75a5365e23a3cac28099c9b025
Tlatoanii <aristavictor14@gmail.com>
2023-09-06 09:28:22 -0600

    Archivos error

--- constants/Images.js
+++ constants/Images.js
@@ -5,15 +5,12 @@ const searchSuperMarket = require('../assets/imgs/searchSuperMarket.png');
 const cart = require('../assets/imgs/cart.png');
 const emptyCart = require('../assets/imgs/emptyCart.png');
 const login = require('../assets/imgs/Login.png');
-<<<<<<< HEAD
 const history = require('../assets/imgs/history.png');
 const history2 = require('../assets/imgs/history2.png');
 const history3 = require('../assets/imgs/history3.png');
 const history4 = require('../assets/imgs/history4.png');
 const history5 = require('../assets/imgs/history5.png');
-=======
 const productNotFound = require("../assets/imgs/productNotFound.png");
->>>>>>> 2fdfe26b462070da83d034bf0e8451b06c8d9258
 export default {
   cart,
   Logo,
@@ -21,13 +18,10 @@ export default {
   searchSuperMarket,
   emptyCart,
   login,
-<<<<<<< HEAD
   history,
   history2,
   history3,
   history4,
   history5,
-=======
   productNotFound
->>>>>>> 2fdfe26b462070da83d034bf0e8451b06c8d9258
 };
